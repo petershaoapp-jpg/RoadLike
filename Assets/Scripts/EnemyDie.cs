aea512ae2c3283b0b1f7552f193d0dbd55123fa9
Jake <228561383+jakelewy@users.noreply.github.com>
2025-09-23 20:17:07 -0700

    Added some health functionality
    also added a debug log for when an enemy dies

--- Assets/Scripts/EnemyDie.cs
+++ Assets/Scripts/EnemyDie.cs
@@ -4,6 +4,7 @@ public class EnemyDie : MonoBehaviour, IDie
 {
     public void OnDie()
     {
+        Debug.Log("[INFO]: Enemy died");
         Destroy(gameObject); // Destroying the attached game object by passing gameObject
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,12 +1,38 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [RequireComponent(typeof(IDie))]
 public class Health : MonoBehaviour
 {
     // TODO (Peter and Jake) (choose whatever you guys want to work on):
-    // - Create a public variable (with a private set value) called health
-    // - Get the die script required by this script
-    // - Create a Heal function that takes in an integer and adds that to the current health value
-    // - Create a TakeDamage function that takes in an integer an removes that from the current health value
-    // Then, check if health is <= 0 (the player has died), and then call the die script's OnDie function
+    // DONE - Create a public variable (with a private set value) called health
+    // DONE - Get the die script required by this script
+    // DONE - Create a Heal function that takes in an integer and adds that to the current health value
+    // - Create a TakeDamage function that takes in an integer and removes that from the current health value
+    //   Then, check if health is <= 0 (the player has died), and then call the die script's OnDie function
+
+    [SerializeField] private int maxHealth = 100; // for customizablility
+    public float health { get; private set; } // public to get, not public to set
+
+    private IDie dieScript;
+
+
+    private void Awake()
+    {
+        health = maxHealth;
+        dieScript = GetComponent<IDie>();
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount <= 0) return;
+
+        health += amount;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    // public void TakeDamage(...
 }
