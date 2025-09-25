74974faa32d697b792bd7ad35efd5e8c64e39587
petershaoapp-jpg <petershaoapp@gmail.com>
2025-09-25 13:06:14 -0700

    Player die testing code

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDie : MonoBehaviour, IDie
 {
+    public KeyCode testDieKey = KeyCode.R;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(testDieKey))
+        {
+            Debug.Log("Test die key pressed! Calling OnDie().");
+            OnDie();
+        }
+    }
+
     public void OnDie()
     {
-        // TODO (Peter): Reset scene when player dies
-        throw new System.NotImplementedException();
+        // Reset scene when player dies
+        SceneManager.LoadScene(0);
+        // The line below was throwing an error and has been removed.
+        // throw new System.NotImplementedException();
     }
 }
