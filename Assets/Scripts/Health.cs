80cc30593d14fc3f9c4ed6aada4d64c332366148
Jake <228561383+jakelewy@users.noreply.github.com>
2025-09-30 10:44:05 -0700

    edit private var name

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,13 +7,13 @@ public class Health : MonoBehaviour
     [SerializeField] private int maxHealth = 100; // for customizablility
     public float health { get; private set; } // public to get, not public to set
 
-    private IDie dieScript;
+    private IDie _dieScript;
 
 
     private void Awake()
     {
         health = maxHealth;
-        dieScript = GetComponent<IDie>();
+        _dieScript = GetComponent<IDie>();
     }
 
     public void Heal(int amount)
@@ -35,7 +35,7 @@ public class Health : MonoBehaviour
         if (health < 0)
         {
             health = 0;
-            dieScript.OnDie();
+            _dieScript.OnDie();
         }
     }
 }
