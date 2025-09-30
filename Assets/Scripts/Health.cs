5c6624f0e1d39b0f057e3f26cda2f64ebed23ec6
Jake <228561383+jakelewy@users.noreply.github.com>
2025-09-30 10:39:48 -0700

    finish health.cs stuff

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof(IDie))]
 public class Health : MonoBehaviour
 {
-    // TODO (Peter and Jake) (choose whatever you guys want to work on):
-    // DONE - Create a public variable (with a private set value) called health
-    // DONE - Get the die script required by this script
-    // DONE - Create a Heal function that takes in an integer and adds that to the current health value
-    // - Create a TakeDamage function that takes in an integer and removes that from the current health value
-    //   Then, check if health is <= 0 (the player has died), and then call the die script's OnDie function
-
     [SerializeField] private int maxHealth = 100; // for customizablility
     public float health { get; private set; } // public to get, not public to set
 
@@ -34,5 +27,15 @@ public class Health : MonoBehaviour
         }
     }
 
-    // public void TakeDamage(...
+    public void TakeDamage(int amount)
+    {
+        if (amount <= 0) return;
+
+        health -= amount;
+        if (health < 0)
+        {
+            health = 0;
+            dieScript.OnDie();
+        }
+    }
 }
